b9b471239816ce10e05b8d874e71c656dc067b5c
deivid7007 <7deividmladenov007@gmail.com>
2022-09-17 01:26:13 +0300

    implemented linq order demo

---
+++ Net7-Demos/LinqOrder.Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿#pragma warning disable
+
+var data = new[] { 2, 1, 3, 6, 5 };
+IEnumerable<int> sorted = null;
+IEnumerable<int> sortedDesc = null;
+
+// NET 6 syntax
+sorted = data.OrderBy(e => e);
+sortedDesc = data.OrderByDescending(e => e);
+
+// NET 7 syntax
+//sorted = data.Order();
+//sortedDesc = data.OrderDescending();
+
+// Printing sorted values
+Console.WriteLine("ASC:");
+foreach (var item in sorted)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("DESC:");
+foreach (var item in sortedDesc)
+{
+    Console.WriteLine(item);
+}
