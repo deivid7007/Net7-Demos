d6921505a2a7708e5c0e62b60c0db358a9ee7390
deivid7007 <7deividmladenov007@gmail.com>
2022-09-26 11:22:36 +0300

    initial linq performance boost demo

---
+++ Net7-Demos/LinqPerformanceBoostDemo/Benchmarks.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace LinqPerformanceBoostDemo
+{
+    [SimpleJob(RuntimeMoniker.Net60)]
+    [SimpleJob(RuntimeMoniker.Net70)]
+    [MemoryDiagnoser(false)]
+    public class Benchmarks
+    {
+        [Params(1000)]
+        public int Size { get; set; }
+
+        public IEnumerable<int> Items { get; set; }
+
+        public void Setup()
+        {
+            Items = Enumerable.Range(1, Size).ToArray();
+        }
+
+        [Benchmark]
+        public int Max() => Items.Max();
+
+        [Benchmark]
+        public int Min() => Items.Min();
+
+        [Benchmark]
+        public double Average() => Items.Average();
+
+        [Benchmark]
+        public int Sum() => Items.Sum();
+    }
+}
---
+++ Net7-Demos/LinqPerformanceBoostDemo/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using LinqPerformanceBoostDemo;
+
+BenchmarkRunner.Run<Benchmarks>();
